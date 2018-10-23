ff88f250daf680f1b9fc61a2492021c7b9bbb24e
winnie <winnie688801@gmail.com>
2018-10-23 11:58:20 +0800

    scanner

--- myproject/src/com/tom/SingUp.java
+++ myproject/src/com/tom/SingUp.java
@@ -13,6 +13,22 @@ public class SingUp {
 		boolean adult = line.toUpperCase().equals("Y");
 		System.out.println(adult);
 		
+		if(adult){
+			System.out.println("How old are you?");
+			int age = scanner.nextInt();
+			scanner.nextLine();
+			System.out.println("What is your name?");
+			String name = scanner.nextLine();
+			System.out.println("What is your nickname?");
+			String nickname = scanner.nextLine();
+			System.out.println("Your information is: "+age+","+name+","+nickname);
+			
+			
+		}else{
+			System.out.println("Come back when you grow to adulthood , Bye");
+			
+		}
+		
 	}
 
 }
