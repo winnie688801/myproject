f6fea1f3df233a903eaaa106e8261b649c306e5f
winnie688801 <winnie688801@gmail.com>
2018-11-27 12:03:12 +0800

    init[]

---
+++ myproject/src/com/tom/PokerGame.java
@@ -0,0 +1,46 @@
+package com.tom;
+
+import java.util.Random;
+
+public class PokerGame {
+
+	public static void main(String[] args) {
+		int[] flowers = {9827 , 9830 , 9829 , 9824};
+		int[] pokers = { 1 , 2 , 3 , 4 , 5 , 6 , 7 , 8 , 9 , 10 , 11 , 12 , 13};
+		for (int i=0; i<pokers.length; i++){
+			System.out.print(pokers);
+		}
+//		int[] nums = new int[5];
+//		nums[0] = 5;
+//		nums[3] = 9;
+//		System.out.println(nums[3]);
+//		System.out.println(nums.length);
+//		for(int i=0; i<nums.length; i++){
+//			System.out.println(nums[i]);
+		}
+//		int poker;
+//		int count = 1;
+//		for(count=1; count<=4; count++){
+//			Random random = new Random();
+//			poker = random.nextInt(13)+1;
+//			int flower = random.nextInt(4);
+//			System.out.print(poker);
+//			
+//			char c = 0;
+//			switch(flower){
+//			case 0:
+//				c = 'c';
+//				break;
+//			case 1:
+//				c = 'D';
+//				break;
+//			case 2:
+//				c = 'H';
+//				break;
+//			case 3:
+//				c = 'S';
+//				break;
+//			}
+//			System.out.println(c);
+//		}
+	}
